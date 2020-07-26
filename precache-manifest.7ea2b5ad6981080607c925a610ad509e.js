489c169ea702fec61be43cbac2d1c8aca5107d80
adrian.briggs.victoria@gmail.com <adrian.briggs.victoria@gmail.com>
2020-07-27 02:19:48 +0800

    Updates

---
+++ precache-manifest.7ea2b5ad6981080607c925a610ad509e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2492d53138ceb025657213d964cfe4b3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "66ae7cf5455efa69cc27",
+    "url": "/monsters-rolodex/static/css/main.602e6536.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ae7cf5455efa69cc27",
+    "url": "/monsters-rolodex/static/js/main.feb92135.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
